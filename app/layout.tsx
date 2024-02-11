22a9d25315920236806597f785280c54234fe15a
a.geraschenko <a.geraschenko@realweb.ru>
2024-02-11 22:18:49 +0300

    feat: тексты

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,12 @@
 import type { Metadata } from "next";
 import { Inter, Kanit } from "next/font/google";
 import "./globals.css";
-import { Header } from "../components/Header";
+// import { Header } from "../components/Header";
 
 const InterFont = Inter({
     subsets: ["latin"],
     variable: '--font-inter',
 });
-
-const KanitFont = Kanit({
-    weight: "800",
-    subsets: ["latin"],
-    variable: '--font-kanit',
-});
-
 
 export const metadata: Metadata = {
     title: "Артур Геращенко",
@@ -28,7 +21,8 @@ export default function RootLayout({
     return (
         <html lang="ru">
         <body className={InterFont.className + ' text-black bg-site'}>
-        <Header/>
+        <div className={'p-4'}>&nbsp;</div>
+        {/*<Header/>*/}
         {children}
         </body>
         </html>
